b3b3848139ac2565c353f39896fb88612b543ad1
Yegor Karpov <1Karpowkin1@gmail.com>
2021-02-11 15:55:27 +0200

    Yegor Animation walk, file sort

---
+++ CvestGameUnity/Assets/MainMenu/UILobby.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UILobby : MonoBehaviour
+{
+    [SerializeField] InputField joinInput;
+    [SerializeField] Button joinButton;
+    [SerializeField] Button hostButton;
+    public void Host()
+    {
+        joinInput.interactable = false;
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+
+        Player_Lobby.localPlayer.HostGame();
+    }
+    public void Join()
+    {
+        joinInput.interactable = false;
+        joinButton.interactable = false;
+        hostButton.interactable = false;
+    }
+}   
