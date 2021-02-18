70e0f2b19dd4cdbf3967efc711fb487cf995d62f
Zans Barinovs <zans.barinovs@gmail.com>
2021-02-18 18:53:39 +0200

    add to ventilationm method check is main character near

---
+++ CvestGameUnity/Assets/Cvests/Cvest1EscapePrison/Ventilation/Scripts/VentilationEnterScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VentilationEnterScript : MonoBehaviour
+{
+    public delegate void CharecterNearVentilationEnterSegmentDelegate();
+    public static event CharecterNearVentilationEnterSegmentDelegate CharecterNearVentilationEnterSegment;
+
+    private float DistanceToCharacter;
+    private bool CharecterIsNear = false.
+    public GameObject MainCharacter;
+    public GameObject EnterPlacePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DistanceToCharacter = Vector3.Distance(MainCharacter.transform.position, EnterPlacePoint.transform.position);
+        if (DistanceToCharacter < (float)1) {
+            CharecterIsNear = true;
+            CharecterNearVentilationEnterSegment?.Invoke();
+        }
+        else
+        {
+            CharecterIsNear = false;
+        }
+    }
+}
