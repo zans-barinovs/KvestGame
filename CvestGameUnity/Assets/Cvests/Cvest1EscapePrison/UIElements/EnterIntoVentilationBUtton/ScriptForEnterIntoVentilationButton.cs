860f2082b7ee3c3a688d5a8f546a65fa18517170
Zans Barinovs <zans.barinovs@gmail.com>
2021-02-26 20:35:54 +0200

    Aded png image as background to EnterIntoVentilationButton

--- CvestGameUnity/Assets/Cvests/Cvest1EscapePrison/UIElements/EnterIntoVentilationBUtton/ScriptForEnterIntoVentilationButton.cs
+++ CvestGameUnity/Assets/Cvests/Cvest1EscapePrison/UIElements/EnterIntoVentilationBUtton/ScriptForEnterIntoVentilationButton.cs
@@ -10,7 +10,7 @@ public class ScriptForEnterIntoVentilationButton : MonoBehaviour
 
     public GameObject ThisObject;
 
-    private float WaitForEventTime = 1.0f;
+    private float WaitForEventTime = 0.5f;
     private float TimerForEventTime = 0.0f;
 
     private float MaxPresedButtonTime = 0.1f;
