5a0fd1987036563b577ab35bc7158715f915c8a1
Zans Barinovs <zans.barinovs@gmail.com>
2021-03-28 12:00:34 +0300

    Add Cvest1 from egors repo

---
+++ CvestGameUnity/Assets/Cvests/Cvest1EscapePrison/Toilet/Scripts/Text_Spawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Text_Spawn : MonoBehaviour
+{
+    public GameObject GameOverText;
+    public GameObject Spaner;
+
+    void Start()
+    {
+        GameOverText.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Spaner.SetActive(true);
+        }
+        GameOverText.SetActive(true);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        GameOverText.SetActive(false);
+    }
+}
