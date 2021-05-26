235aa2e77cc9b53e9747b69fe15819b9831a5607
Zans Barinovs <zans.barinovs@gmail.com>
2021-05-26 23:30:01 +0300

    renamed player to FirstMainCharacter. Created GameNetworkScript.

---
+++ CvestGameUnity/Assets/MainMenu/Scripts/GameNetworkScript.cs
@@ -0,0 +1,33 @@
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameNetworkScript : MonoBehaviourPunCallbacks
+{
+    private void Start() 
+    {
+        PhotonNetwork.Instantiate{}; //видео 15:13 Указать название игрока и его координаты спауна для руна.
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+    }
+
+    public override void OnLeftRoom() //вызывается когда этот игролк покидает комнату.
+    {
+        SceneManager.LoadScene(0);
+        Debug.Log("This Player left room");
+    }
+
+    public override void OnPlayerEnteredRoom(Player NewPlayer)
+    {
+        Debug.LogFormat("Player {0} entered room", NewPlayer.NickName);
+    }
+
+    public override void OnPlayerLeftRoom(Player LeftPlayer)
+    {
+        Debug.LogFormat("Player {0} left room", LeftPlayer.NickName);
+    }
+}
